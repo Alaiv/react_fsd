6a9b9f453cb295bce1d199bbc96716c1ca5a5644
Aleks <lasacc2022@gmail.com>
2024-06-21 00:31:31 +0300

    screenshots

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,44 @@
+import '@testing-library/jest-dom';
+
+import { screen } from '@testing-library/react';
+import { userEvent } from '@storybook/testing-library';
+import { StateSchema } from 'app/providers/storeProvider';
+import { RenderForTest } from 'shared/lib/tests/renderForTest';
+import * as React from 'react';
+import { Counter } from './Counter';
+
+describe('Counter.test', () => {
+    test('all elements are showed', () => {
+        const state: StateSchema = {
+            counter: { value: 5 },
+        };
+
+        RenderForTest(<Counter />, { initialState: state });
+
+        expect(screen.getByTestId('counter-val')).toHaveTextContent('5');
+        expect(screen.getByTestId('inc')).toBeInTheDocument();
+        expect(screen.getByTestId('dec')).toBeInTheDocument();
+    });
+
+    test('increment', () => {
+        const state: StateSchema = {
+            counter: { value: 5 },
+        };
+
+        RenderForTest(<Counter />, { initialState: state });
+
+        userEvent.click(screen.getByTestId('inc'));
+        expect(screen.getByTestId('counter-val')).toHaveTextContent('6');
+    });
+
+    test('decrement', () => {
+        const state: StateSchema = {
+            counter: { value: 5 },
+        };
+
+        RenderForTest(<Counter />, { initialState: state });
+
+        userEvent.click(screen.getByTestId('dec'));
+        expect(screen.getByTestId('counter-val')).toHaveTextContent('4');
+    });
+});
